d40bdd3d7c7129d50e0b70e8438e4d626e1a2593
Ali Hussan <malikalihussan2@gmail.com>
2024-09-04 08:34:32 -0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const{add,minus}= require('./math')
-let addnNumbers=add(2,5);
-let minusnNumbers=minus(4,2);
+const fs=require('fs')
+// const{add,minus}= require('./math')
+// let addnNumbers=add(2,5);
+// let minusnNumbers=minus(4,2);
 
-console.log(addnNumbers)
-console.log(minusnNumbers)
+// console.log(addnNumbers)
+// console.log(minusnNumbers)
+// fs.readFile("html.txt", "Utf8", function(err, data){
+
+// if 
+// (err) {console.log(err)}
+// else
+// {console.log(data)}
+// })
+
+//To write something in file.
+// let data="Ali Hussan Malik(The Loely BOy)"
+fs.writeFile("html.txt", "The Lovely BOy" , (err)=>{
+
+    if (err) {
+        console.log(err)
+    }
+    else{
+    console.log(fs.readFileSync("html.txt","utf8") )
+}
+    })
